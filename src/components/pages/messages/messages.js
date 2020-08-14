128b94167e4e866221768a9eeb164d40dccae57a
Mikhail <your_email@whatever.comlukyanenkomikhail@gmail.com>
2020-08-14 17:38:58 +0300

    lesson 40/41

--- src/components/pages/messages/messages.js
+++ src/components/pages/messages/messages.js
@@ -7,13 +7,20 @@ import MessageItemsItem from './messagesItemItem/messagesItemItem';
 const Messages = (props)=>{
     let dialogsElements = props.dialogs.map(dialog =><DialogsItem name={dialog.name} id={dialog.id} />);
     let messagesElements = props.messages.map(message =><MessageItemsItem message={message.message}/>)
+    let onSendMessageClick = () =>{
+
+    }
     return (
         <div className="messages">
             <div className='dialogs'>
                 {dialogsElements}
             </div>
             <div className='message__items'>
-                {messagesElements}
+                <div>{messagesElements}</div>
+                <div>
+                    <div><textarea placeholder="Enter your message here"></textarea></div>
+                    <div><button onClick={onSendMessageClick}>SEND</button></div>
+                </div>
             </div>
         </div>
     )
--- src/data/state.js
+++ src/data/state.js
@@ -1,3 +1,10 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_TEXT_POST = 'UPDATE-NEW-TEXT-POST';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
+
+
+
 let store = {
         _state: {
                 profilePage: {
@@ -19,6 +26,8 @@ let store = {
                             {'id':'3', 'message':'This is third message'}, 
                             {'id':'4', 'message':'I am realy cool!'},
                             ], 
+                        newMessageBody: '',
+
                         dialogs: 
                         [
                              {'id':'1', 'name':'Anna'},
@@ -44,7 +53,7 @@ let store = {
 
 
         dispatch(action) {
-                if(action.type ==='ADD-POST') {
+                if(action.type ===ADD_POST) {
                         let newPost = {
                                 id: 5,
                                 message: this._state.profilePage.newPostText,
@@ -53,25 +62,33 @@ let store = {
                             this._state.profilePage.posts.push(newPost);
                             this._state.profilePage.newPostText = '';
                             this._callSubscriber(this._state);
-                }else if(action.type==='UPDATE-NEW-TEXT-POST') {
+                }else if(action.type===UPDATE_NEW_TEXT_POST) {
                         this._state.profilePage.newPostText = action.newText;
+                        this._callSubscriber(this._state);
+                }else if(action.type===UPDATE_NEW_MESSAGE_BODY) {
+                        this._state.messagesPage.newMessageBody = action.body;
+                        this._callSubscriber(this._state);
+                }else if(action.type===SEND_MESSAGE) {
+                        let body = this._state.messagesPage.newMessageBody;
+                        this._state.messagesPage.messages.push({id: 6, message: body});
                         this._callSubscriber(this._state);
                 }
 
         } 
 }
 
-export let addPostActionCreator = ()=>{
+export const addPostActionCreator = ()=>{
         return {
             type: 'ADD-POST'
         }
     }
-export let updateNewPostActionCreator = (text) =>{
+export const updateNewPostActionCreator = (text) =>{
         return (
             {type:'UPDATE-NEW-TEXT-POST',
             newText: text
             }
         )
     }
-
+export const sendMessageActionCreator = ()=> ({type: SEND_MESSAGE})
+export const updateNewMessageBodyActionCreator = (body) => ({type: UPDATE_NEW_MESSAGE_BODY, body: body})
 export default store;
