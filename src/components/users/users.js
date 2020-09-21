efeec26475e4c8ee473c08058f3bd6bf4347adde
Mikhail <your_email@whatever.comlukyanenkomikhail@gmail.com>
2020-09-21 13:50:15 +0300

    56 users become a function

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,81 +1,46 @@
 import React from 'react';
 import './users.css';
-import * as axios from 'axios';
 
-class Users extends React.Component {
 
-    getUsers = ()=>{
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-        //https://social-network.samuraijs.com/api/1.0/users?page=99&count=2
-        .then(response => {this.props.setUsers(response.data.items);
-                           this.props.setTotalUsersQuantity(response.data.totalCount);          
+let Users = (props) => {
+    let pagesQuantity = Math.ceil(props.totalUsersQuantity / props.pageSize);
+    let pages = [];
+    for (let i=1; i<=pagesQuantity; i++) {
+        pages.push(i);
+    }
+
+    return <div className='users'>
+    <div>
+        {pages.map(p => {
+            return <span key = {p} className={(props.currentPage === p)? 'selectedPage' : 'currentPage'}  
+            onClick={(e)=> { props.onPageChanged(p) } }
+            >{p}</span>
         })
-/*         if (this.props.users.length === 0) {
-            this.props.setUsers([
-                {id: 1, photoUrl: 'https://pm1.narvii.com/6889/74979d4d2744ec6e27995b6e866f091d04c0b40cr1-515-414v2_uhq.jpg', followed: false, fullName: 'Alex', status: 'boss', location: {city: 'Moskow', country: 'Russia'} },
-                {id: 2, photoUrl: 'https://g0.sunmag.me/sunmag.me/wp-content/uploads/2019/11/sunmag-004-small-avatar.png', followed: true, fullName: 'Boris', status: 'second boss', location: {city: 'Kiev', country: 'Ukraine'} },
-                {id: 3, photoUrl: 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png', followed: false, fullName: 'Charlie', status: 'I am a boss too', location: {city: 'Minsk', country: 'Belarus'} }
-            ])
-        }  */
-    }
-
-    componentDidMount() {
-        this.getUsers();
-    }
-
-    onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-        .then(response => {
-            this.getUsers();
-            this.props.setTotalUsersQuantity(response.data.totalCount);
-        })
-    }
-   
-    render() {
-
-        let pagesQuantity = Math.ceil(this.props.totalUsersQuantity / this.props.pageSize);
-        let pages = [];
-        for (let i=1; i<=pagesQuantity; i++) {
-            pages.push(i);
         }
-       
-
-         return <div className='users'>
+    </div>
+{
+    props.users.map( u=> <div key={u.id}>
+        <span>
             <div>
-                {pages.map(p => {
-                    return <span className={(this.props.currentPage === p)? 'selectedPage' : 'currentPage'}
-                    
-                    onClick={(e)=> { this.onPageChanged(p) } }
-                    >{p}</span>
-                })
-                }
+                <img src={u.photos.large || u.photos.small || 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png'} className='userPhoto' alt='аватарка пользователя'/>
             </div>
-        {
-            this.props.users.map( u=> <div key={u.id}>
-                <span>
-                    <div>
-                        <img src={u.photos.large || u.photos.small || 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png'} className='userPhoto' alt='аватарка пользователя'/>
-                    </div>
-                    <div>
-                        {u.followed 
-                            ? <button onClick={()=>{this.props.unfollow(u.id)}}>Unfollow</button> 
-                            : <button onClick={()=>{this.props.follow(u.id)}}>Follow</button>}
-                    </div>
-                </span>
-                <span>
-                    <div>{u.name}</div>
-                    <div>{u.status}</div>
-                </span>
-                <span>
-                    <div>{'u.location.country'}</div>
-                    <div>{'u.location.city'}</div>
-                </span>
-            </div> )
-        }
-    </div> 
-
-    }
+            <div>
+                {u.followed 
+                    ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button> 
+                    : <button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+            </div>
+        </span>
+        <span>
+            <div>{u.name}</div>
+            <div>{u.status}</div>
+        </span>
+        <span>
+            <div>{'u.location.country'}</div>
+            <div>{'u.location.city'}</div>
+        </span>
+    </div> )
+}
+</div> 
 }
 
 
--- src/components/users/usersContainer.js
+++ src/components/users/usersContainer.js
@@ -2,6 +2,52 @@ import React from 'react';
 import Users from './users';
 import {connect} from 'react-redux';
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersQuantityAC } from '../../data/users-reducer';
+import * as axios from 'axios';
+
+class UsersContainer extends React.Component {
+
+    getUsers = ()=>{
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+        //https://social-network.samuraijs.com/api/1.0/users?page=99&count=2
+        .then(response => {this.props.setUsers(response.data.items);
+                           this.props.setTotalUsersQuantity(response.data.totalCount);          
+        })
+/*         if (this.props.users.length === 0) {
+            this.props.setUsers([
+                {id: 1, photoUrl: 'https://pm1.narvii.com/6889/74979d4d2744ec6e27995b6e866f091d04c0b40cr1-515-414v2_uhq.jpg', followed: false, fullName: 'Alex', status: 'boss', location: {city: 'Moskow', country: 'Russia'} },
+                {id: 2, photoUrl: 'https://g0.sunmag.me/sunmag.me/wp-content/uploads/2019/11/sunmag-004-small-avatar.png', followed: true, fullName: 'Boris', status: 'second boss', location: {city: 'Kiev', country: 'Ukraine'} },
+                {id: 3, photoUrl: 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png', followed: false, fullName: 'Charlie', status: 'I am a boss too', location: {city: 'Minsk', country: 'Belarus'} }
+            ])
+        }  */
+    }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+        .then(response => {
+            this.getUsers();
+            this.props.setTotalUsersQuantity(response.data.totalCount);
+        })
+    }
+   
+    render() {
+        return <Users 
+            totalUsersQuantity = {this.props.totalUsersQuantity}
+            pageSize = {this.props.pageSize}
+            currentPage = {this.props.currentPage}
+            onPageChanged = {this.onPageChanged}
+            unfollow = {this.props.unfollow}
+            follow = {this.props.follow}
+            users = {this.props.users}
+
+
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -32,4 +78,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
