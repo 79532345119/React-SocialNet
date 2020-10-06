fb804983a5959733529e598c5413e548187a6413
Mikhail <your_email@whatever.comlukyanenkomikhail@gmail.com>
2020-10-06 17:12:05 +0300

    change BrowserRouter basename={process.env.PUBLIC_URL} to HashRouter

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 
 
   ReactDOM.render(
     <React.StrictMode>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Provider store = {store}>
           <App />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
   )
