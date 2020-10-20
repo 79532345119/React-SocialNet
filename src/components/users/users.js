b6df83d483797b8e44180b2ab7c2e9fb368456af
Mikhail <your_email@whatever.comlukyanenkomikhail@gmail.com>
2020-10-20 12:31:52 +0300

    extract components UsersCounter and UserToShow in users.js

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -3,8 +3,16 @@ import { NavLink } from 'react-router-dom';
 import './users.css';
 
 
-
 let Users = (props) => {
+
+    return <div className='users'>
+                <UsersCounter totalUsersQuantity={props.totalUsersQuantity} pageSize={props.pageSize} currentPage={props.currentPage} onPageChanged={props.onPageChanged} />
+
+                <UsersToShow users={props.users} isFollowingInProgress={props.isFollowingInProgress} unfollow={props.unfollow} follow={props.follow } />
+            </div> 
+}
+
+export const UsersCounter = (props) => {
     let pagesQuantity = Math.ceil(props.totalUsersQuantity / props.pageSize);
     let pages = [];
     for (let i=1; i<=pagesQuantity; i++) {
@@ -13,57 +21,47 @@ let Users = (props) => {
     
     let selectedPages = pages.slice(props.currentPage-5 >0 ? props.currentPage-5: 0
         , props.currentPage-5 >0 ? props.currentPage+5 : 10);
+        
+    return <div className='usersQuantity'>
+                {selectedPages.map(p => {
+                    return <span key = {p} className={(props.currentPage === p)? 'selectedPage' : 'currentPage'}  
+                    onClick={(e)=> { props.onPageChanged(p) } }
+                    >{p}</span>
+                })
+                }
+                <span>TOTAL {pagesQuantity} PAGES</span>
+            </div>
 
-    return <div className='users'>
-    <div className='usersQuantity'>
-        {selectedPages.map(p => {
-            return <span key = {p} className={(props.currentPage === p)? 'selectedPage' : 'currentPage'}  
-            onClick={(e)=> { props.onPageChanged(p) } }
-            >{p}</span>
-        })
-        }
-        <span>TOTAL {pagesQuantity} PAGES</span>
-    </div>
-{
-    props.users.map( u=> <div  className = 'userField' key={u.id}>
-        <div>
-            <div>
-                <NavLink to={'/profile/'+u.id}>
-                    <img src={u.photos.large || u.photos.small || 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png'} className='userPhoto' alt='аватарка пользователя'/>
-                </NavLink>
-            </div>
-            <div>
-                {u.followed 
-                    ? <button className='btn-sm' disabled={props.isFollowingInProgress.some(id => id ===u.id)} 
-                        onClick={()=>{
-                            props.unfollow(u.id)
-/*                        props.toggleIsFollowingInProgress(true, u.id)
-                        usersAPI.unfollowUser(u.id)
-                            .then(data => {
-                                    if (data.resultCode === 0) {props.unfollow(u.id)};
-                                    props.toggleIsFollowingInProgress(false, u.id);
-                                })  */
-                        }}>Unfollow</button> 
-                    : <button className='btn-sm' disabled={props.isFollowingInProgress.some(id => id ===u.id)} 
-                        onClick={()=>{
-                            props.follow(u.id)
-/*                        props.toggleIsFollowingInProgress(true, u.id)
-                        usersAPI.followUser(u.id)
-                                .then(data => {
-                                    if (data.resultCode === 0) {props.follow(u.id)};
-                                    props.toggleIsFollowingInProgress(false, u.id)
-                                }) */
-                        }}>Follow</button>}
-            </div>
-        </div>
-        <div>
-            <div className='userName'>{u.name}</div>
-            <div>{u.status ? u.status : 'too lazy to add status'}</div>
-            <div>user ID {u.id}</div>
-        </div>
-    </div> )
 }
-</div> 
+
+export const UsersToShow = (props) => {
+    return (
+        props.users.map( u=> <div  className = 'userField' key={u.id}>
+            <div>
+                <div>
+                    <NavLink to={'/profile/'+u.id}>
+                        <img src={u.photos.large || u.photos.small || 'https://shapka-youtube.ru/wp-content/uploads/2018/10/spartan.png'} className='userPhoto' alt='аватарка пользователя'/>
+                    </NavLink>
+                </div>
+                <div>
+                    {u.followed 
+                        ? <button className='btn-sm' disabled={props.isFollowingInProgress.some(id => id ===u.id)} 
+                            onClick={()=>{
+                                props.unfollow(u.id)
+                            }}>Unfollow</button> 
+                        : <button className='btn-sm' disabled={props.isFollowingInProgress.some(id => id ===u.id)} 
+                            onClick={()=>{
+                                props.follow(u.id)
+                            }}>Follow</button>}
+                </div>
+            </div>
+            <div>
+                <div className='userName'>{u.name}</div>
+                <div>{u.status ? u.status : 'too lazy to add status'}</div>
+                <div>user ID {u.id}</div>
+            </div>
+        </div> )
+    )
 }
 
 
